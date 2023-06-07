1a49068739d94c950d409750d233b7d9a2f681bb
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-06-07 00:42:06 +0000

    Create defaultDict.py

---
+++ defaultDict.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+n,m = map(int, input().split())
+
+indicies = defaultdict(list)
+
+for i in range(1, n+1):
+    word = input()
+    indicies[word].append(i)
+    
+for i in range(1, m+1):
+    word = input()
+    if word in indicies:
+        print(indicies[word])
+    else:
+        print(-1)
