c14638bb10d3846c16dfb7caff87da3d202663bc
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-06-08 10:21:54 +0000

    Create wordCount.py

---
+++ wordCount.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+word_count = {}
+
+for i in range(n):
+    word = input().strip()
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+print(len(word_count))
+
+for word in word_count:
+    print(word_count[word], end=" ")
+    
