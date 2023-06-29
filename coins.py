ab2b537949505110331f8083ce7afd030463b41d
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-06-29 22:58:48 +0000

    Create coins.py

---
+++ coins.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+
+    if (n % 2 == 0) or (n % k == 0):
+        print("YES")
+    elif (n-k) % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
