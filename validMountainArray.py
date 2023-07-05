6b14aaf58269d66abbd00f165b1a40f0c86d6475
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-07-05 00:53:15 +0000

    Create validMountainArray.py

---
+++ validMountainArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        pointer = 0
+
+        while pointer < len(arr) - 1 and arr[pointer] < arr[pointer+1]:
+            pointer += 1
+
+        if pointer == 0 or pointer == len(arr) - 1:
+            return False
+
+        while pointer < len(arr) - 1 and arr[pointer] > arr[pointer+1]:
+            pointer += 1
+
+        return pointer == len(arr) - 1
