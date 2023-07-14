c07aae21ce262ef6a9f2cb54d983115244dd23e5
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-07-14 15:53:11 +0000

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -2,6 +2,8 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         l = 1
 
+        if len(nums) == 0: return 0
+
         for r in range(1, len(nums)):
             if nums[r] != nums[r-1]:
                 nums[l] = nums[r]
