7beaf69b37705f1d7277c36c12e99e20a6a6c9f7
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-08-10 15:12:29 +0000

    Create longestConsecutive.py

---
+++ A2SV-Problem-Solving/longestConsecutive.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        seq_map = set(nums)
+        longest = 0
+
+        for n in nums:
+            if (n-1) not in seq_map:
+                length = 0
+                while (n + length) in seq_map:
+                    length += 1
+                longest =max (length, longest)
+        return longest
+
+
