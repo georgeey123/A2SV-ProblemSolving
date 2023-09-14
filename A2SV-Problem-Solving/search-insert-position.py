d749793b0d1df072cfa480040b47e07e88a08166
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-09-14 11:50:29 +0000

    Update search-insert-position.py

--- A2SV-Problem-Solving/search-insert-position.py
+++ A2SV-Problem-Solving/search-insert-position.py
@@ -1,3 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect_left(nums, target)
+        # return bisect_left(nums, target)
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = left + (right-left) // 2
+            
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
