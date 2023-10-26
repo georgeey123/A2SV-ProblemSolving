7cda059871ec0a30f3e1d5981b914f67ce795c77
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-10-26 20:14:05 +0000

    Added solution for Reverse Linked List

--- A2SV-Problem-Solving/reverse-linked-list.py
+++ A2SV-Problem-Solving/reverse-linked-list.py
@@ -5,13 +5,11 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        prev = None
-        cur = head
+        if not head or not head.next:
+            return head
 
-        while cur:
-            next_node = cur.next
-            cur.next = prev
-            prev = cur  
-            cur = next_node
-        return prev
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return new_head
