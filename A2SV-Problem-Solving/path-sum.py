7a315f747b9ef369a0ebd4689c8519c132bd9de8
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2023-11-09 17:24:32 +0000

    Added solution for Path Sum

---
+++ A2SV-Problem-Solving/path-sum.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        def path(root, cur_sum):
+            if not root:
+                return False
+
+            cur_sum += root.val
+
+            if not root.left and not root.right:
+                return cur_sum == targetSum
+
+            return path(root.left, cur_sum) or path(root.right, cur_sum)
+
+        return path(root, 0)
