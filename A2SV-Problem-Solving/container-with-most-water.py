c44a6ca6465b9fd2bf9e1f0a997c95edc5778519
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-02-13 16:04:15 +0000

    Added solution for Container With Most Water

--- A2SV-Problem-Solving/container-with-most-water.py
+++ A2SV-Problem-Solving/container-with-most-water.py
@@ -1,17 +1,8 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        '''
+        
         res = 0
-
-        for l in range(len(height)):
-            for r in range(l+1, len(height)):
-                area = ((r-l) * min(height[l], height[r]))
-                res = max(res, area)
-        return
-        '''
-
-        res = 0
-        l, r = 0, len(height) -1
+        l, r = 0, len(height) - 1
 
         while l < r:
             area = ((r-l) * min(height[l], height[r]))
@@ -21,5 +12,5 @@ class Solution:
                 l += 1
             else:
                 r -= 1
+
         return res
-       
