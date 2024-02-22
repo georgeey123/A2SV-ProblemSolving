8b68614fcbd5706fd8b3848fbd3a62276b3f5976
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-02-22 21:37:25 +0000

    Added solution for Get Maximum in Generated Array

--- A2SV-Problem-Solving/get-maximum-in-generated-array.py
+++ A2SV-Problem-Solving/get-maximum-in-generated-array.py
@@ -1,24 +1,40 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
         nums = [-1] * (n+1)
-        # nums[0] = 0
-        # nums[1] = 1
 
+        # def dp(i):
+        #     if i < 2:
+        #         nums[i] = i
+        #         return nums[i]
 
-        def dp(i):
-            if i < 2:
-                nums[i] = i
-                return nums[i]
+        #     if nums[i] == -1:
+        #         if i % 2 == 0:
+        #             nums[i] = dp((i)//2)
+        #         else:
+        #             nums[i] = dp((i-1)//2) + dp((i-1)//2 + 1)
 
-            if nums[i] == -1:
+        #     return nums[i]
+
+        # for i in range(n, -1, -1):
+        #     dp(i)
+        # return max(nums)
+
+        dp = [-1] * (n + 1)
+
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1 
+
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            if dp[i] == -1:
                 if i % 2 == 0:
-                    nums[i] = dp((i)//2)
+                    dp[i] = dp[i // 2]
                 else:
-                    nums[i] = dp((i-1)//2) + dp((i-1)//2 + 1)
+                    dp[i] = dp[i // 2] + dp[(i // 2) + 1]
 
-            return nums[i]
-
-        for i in range(n, -1, -1):
-            dp(i)
-        return max(nums)
+        return max(dp)
 
