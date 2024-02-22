8ef49d8552006a8aa80de5682d5da83f4592889f
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-02-22 15:12:16 +0000

    Added solution for Coin Change

--- A2SV-Problem-Solving/coin-change.py
+++ A2SV-Problem-Solving/coin-change.py
@@ -1,21 +1,33 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
+        # memo = {}
 
-        def dp(i):
-            if i == 0:
-                return 0
-            if i < 0:
-                return float('inf')
+        # def dp(i):
+        #     if i == 0:
+        #         return 0
+        #     if i < 0:
+        #         return float('inf')
 
-            mn = float('inf')
+        #     mn = float('inf')
 
-            if i not in memo:
-                for coin in coins:
-                    mn = min(mn, 1 + dp(i-coin))
-                    memo[i] = mn
-            return memo[i]
+        #     if i not in memo:
+        #         for coin in coins:
+        #             mn = min(mn, 1 + dp(i-coin))
+        #             memo[i] = mn
+        #     return memo[i]
 
-        res = dp(amount)
+        # res = dp(amount)
 
-        return res if res != float('inf') else -1
+        # return res if res != float('inf') else -1
+
+        dp = [amount + 1] * (amount+1)
+        dp[0] = 0
+
+        for i in range(1, len(dp)):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i-coin])
+
+        res = dp[amount]
+
+        return res if res != amount + 1 else -1
