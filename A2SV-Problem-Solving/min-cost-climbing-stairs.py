ea0f6ecdbba5ec96afb686a544e7da082f9a8422
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-02-22 16:47:41 +0000

    Added solution for Min Cost Climbing Stairs

--- A2SV-Problem-Solving/min-cost-climbing-stairs.py
+++ A2SV-Problem-Solving/min-cost-climbing-stairs.py
@@ -1,15 +1,24 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = {}
+        # memo = {}
 
-        def dp(idx):
-            if idx >= len(cost):
-                return 0
+        # def dp(idx):
+        #     if idx >= len(cost):
+        #         return 0
 
-            if idx not in memo:
-                one_step, two_step = dp(idx+1), dp(idx+2)
-                memo[idx] = cost[idx] + min(one_step, two_step)
+        #     if idx not in memo:
+        #         one_step, two_step = dp(idx+1), dp(idx+2)
+        #         memo[idx] = cost[idx] + min(one_step, two_step)
 
-            return memo[idx]
+        #     return memo[idx]
 
-        return min(dp(0),dp(1))
+        # return min(dp(0),dp(1))
+
+        dp = [0] * (len(cost)+1)
+        dp[-2] = dp[-3] = cost[-1]
+
+        for i in range(len(cost)-2, -1, -1):
+            dp[i] = cost[i] + min(dp[i+1], dp[i+2])
+
+        return min(dp[0], dp[1])
+
