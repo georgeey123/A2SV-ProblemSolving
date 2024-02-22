0b870b6691e0fa5594817d2b469ff955cac0f3e0
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-02-22 15:57:18 +0000

    Added solution for N-th Tribonacci Number

--- A2SV-Problem-Solving/n-th-tribonacci-number.py
+++ A2SV-Problem-Solving/n-th-tribonacci-number.py
@@ -1,16 +1,30 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = {}
+        # memo = {}
 
-        def dp(idx):
-            if idx == 0:
-                return 0
-            if idx == 1 or idx == 2:
-                return 1
+        # def dp(idx):
+        #     if idx == 0:
+        #         return 0
+        #     if idx == 1 or idx == 2:
+        #         return 1
 
-            if idx not in memo:
-                memo[idx] = dp(idx-1) + dp(idx-2) + dp(idx-3)
+        #     if idx not in memo:
+        #         memo[idx] = dp(idx-1) + dp(idx-2) + dp(idx-3)
             
-            return memo[idx]
+        #     return memo[idx]
 
-        return dp(n)
+        # return dp(n)
+
+        dp = [0] * (n+1)
+        
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+
+        dp[0], dp[1], dp[2] = 0, 1, 1
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+        return dp[-1]
