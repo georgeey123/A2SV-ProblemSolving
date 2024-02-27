2f5da227f9aca99acfee824644a7f43c8328e5ec
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-02-27 16:04:35 +0000

    Added solution for Predict the Winner

--- A2SV-Problem-Solving/predict-the-winner.py
+++ A2SV-Problem-Solving/predict-the-winner.py
@@ -1,16 +1,19 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
+        memo = {}
 
         def score(left, right):
 
             if left > right:
                 return 0
 
-            player1_score = nums[left] - score(left + 1, right)
-            player2_score = nums[right] - score(left, right - 1)
+            state = (left, right)
+            if state not in memo:
+                player1_score = nums[left] - score(left + 1, right)
+                player2_score = nums[right] - score(left, right - 1)
+                memo[state] = max(player1_score, player2_score)
 
-
-            return max(player1_score, player2_score)
+            return memo[state]
 
         return score(0, n-1) >= 0
