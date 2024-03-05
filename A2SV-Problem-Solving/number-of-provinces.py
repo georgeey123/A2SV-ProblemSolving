3638765378f97811d3c5b2dd369bbc03d00adb15
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-03-05 15:44:40 +0000

    Added solution for Number of Provinces

--- A2SV-Problem-Solving/number-of-provinces.py
+++ A2SV-Problem-Solving/number-of-provinces.py
@@ -1,28 +1,49 @@
+class UnionFind:
+
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+        self.size = [1] * size
+
+    def find(self, x):
+        # if x == self.parent[x]:
+        #     return x
+        # self.parent[x] = self.find(self.parent[x])
+        # return self.parent[x]
+        root = x
+
+        while root != self.parent[root]:
+            root = self.parent[root]
+
+        while x != root:
+            parent = self.parent[x]
+            self.parent[x] = root
+            x = parent
+        return root
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+
+        if rootx != rooty:
+            if self.size[rootx] > self.size[rooty]:
+                self.parent[rooty] = rootx
+                self.size[rootx] += self.size[rooty]
+            else:
+                self.parent[rootx] = rooty
+                self.size[rooty] += self.size[rootx]
+        
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        rows, cols = len(isConnected), len(isConnected[0])
-
-        for i in range(rows):
-            for j in range(cols):
-                if isConnected[i][j] == 1:
-                    graph[i+1].append(j+1)
-
-        visited = set()
-        def dfs(node, visited):
-            if node in visited:
-                return False
-
-            visited.add(node)
-            for neighbour in graph[node]:
-                if neighbour not in visited:
-                    a = dfs(neighbour, visited)
-                    if a:
-                        return a
-
-        count = 0
-        for start in graph:
-            if dfs(start, visited) == None:
-                count += 1
+        n = len(isConnected)
+        unionF = UnionFind(n)
+        count = n
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1 and unionF.find(i) != unionF.find(j):
+                    count -= 1
+                    unionF.union(i,j)
 
         return count
+
+
+
