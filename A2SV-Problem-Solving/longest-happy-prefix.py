ea2216298fa76581e5f76c7613ed759ce2e52c8a
Ohene-Adjei George Kofi <georgeey123@gmail.com>
2024-04-04 23:28:38 +0000

    Added solution for Longest Happy Prefix

--- A2SV-Problem-Solving/longest-happy-prefix.py
+++ A2SV-Problem-Solving/longest-happy-prefix.py
@@ -1,16 +1,24 @@
 class Solution:
     def longestPrefix(self, s: str) -> str:
-        n = len(s)
-        lps = [0] * n
+        base = 31
+        m = len(s)
+        mod = ((10 ** 9)+7)
+        res = ""
+        hash1 = (ord(s[0]) - 96) % mod
+        hash2 = (ord(s[-1]) - 96) % mod 
 
-        for i in range(1, n):
-            j = lps[i-1]
-            while j > 0 and s[i] != s[j]:
-                j = lps[j-1]
+        for i in range(1, len(s)-1):
+            if hash1 == hash2: res = s[:i]
 
-            if s[i] == s[j]:
-                j += 1
-            
-            lps[i] = j
+            hash1 *= 31
+            hash1 += (ord(s[i])-96)
+            hash1 %= mod
 
-        return "".join(s[:lps[-1]]) if lps[-1] != 0 else ""
+            hash2 += ((ord(s[m-i-1])-96) * base)
+            hash2 %= mod
+            base *= 31
+            base %= mod
+
+        if hash1 == hash2: res = s[:m-1]
+
+        return res
